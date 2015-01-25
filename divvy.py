7e2f74b5a390160be681daffe507588ae9ece463
cambyses <kambiz.behi@post.harvard.edu>
2015-01-25 11:20:55 -0600

    iteration

--- divvy.py
+++ divvy.py
@@ -11,3 +11,11 @@ data = urlopen(webservice_url).read().decode("utf8")
 result = json.loads(data)
 stations = result['stationBeanList']
 print(stations)
+
+count = 0
+x1 = 41.793414
+y1 = -87.600915
+
+#stores the closest city after each iteration.
+station_number = 0 
+
